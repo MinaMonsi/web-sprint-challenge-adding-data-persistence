4156dff7e9220d70c2d04d723b46e95b2ad4fcc9
Marina Monsivais <minamonsi@me.com>
2021-10-23 18:52:05 -0500

    Server up and running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // build your server here and require it from index.js
+const express = require ("express");
+const server = express();
+
+
+
+server.use(express.json());
+
+const currentTime = new Date().toLocaleTimeString();
+
+server.get('/status', (req, res)=>{
+    res.status(200).json({
+        status: 200,
+        message: `Server is up and running at ${currentTime}`,
+    });
+})
+
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// start your server here
+require('dotenv').config()
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => {
+  console.log(`\nServer is running on port ${PORT} in {process.env.NODE_ENV} mode!\n`);
+});
