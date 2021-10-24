0d195a16068a8d12328e7cca183fe3537bea3e3f
Marina Monsivais <minamonsi@me.com>
2021-10-24 17:02:23 -0500

    WIP

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,18 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+function getResourceById(){
+    return db('resource');
+}
+
+async function addResource(resource){
+    await db('resource').insert(resource)
+    .then(() => {
+        return db('resource')
+        .select('resource.resource_name')
+        .first()
+    })
+}
+module.export = {
+    getResourceById,
+    addResource
+}
